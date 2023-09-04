5adad7859db894c850423c8a6b4e348b55c25ce9
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2023-09-04 08:25:06 -0300

    Transaction Model

---
+++ src/main/java/com/david/simplepay/entities/Transaction.java
@@ -0,0 +1,31 @@
+package com.david.simplepay.entities;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Table(name = "transactions")
+@Entity(name = "transactions")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @JoinColumn(name = "receiver_id")
+    @ManyToOne
+    private User receiver;
+    private LocalDateTime createdAt;
+}
